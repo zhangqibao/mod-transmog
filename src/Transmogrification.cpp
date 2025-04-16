2ea72f2c940990b2cc1f1200270a411f18cbcb8e
zhangqibao <zhangqibao@users.noreplay.github.com>
2025-04-17 00:03:18 +0800

    修复最新AZ代码兼容问题

--- src/Transmogrification.cpp
+++ src/Transmogrification.cpp
@@ -3,6 +3,8 @@
 #include "DatabaseEnv.h"
 #include "SpellMgr.h"
 #include "Tokenize.h"
+#include "WorldSession.h"
+#include "WorldSessionMgr.h"
 
 Transmogrification* Transmogrification::instance()
 {
@@ -1079,8 +1081,8 @@ void Transmogrification::LoadConfig(bool reload)
 
     if (reload) // dont store presets for nothing
     {
-        SessionMap const& sessions = sWorld->GetAllSessions();
-        for (SessionMap::const_iterator it = sessions.begin(); it != sessions.end(); ++it)
+        WorldSessionMgr::SessionMap const& sessions = sWorldSessionMgr->GetAllSessions();
+        for (WorldSessionMgr::SessionMap::const_iterator it = sessions.begin(); it != sessions.end(); ++it)
         {
             if (Player* player = it->second->GetPlayer())
             {
